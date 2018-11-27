cf93151d3a37fdb8131a3b0651a2ef4a5b6cecfe
Henry Shilling <bigdaddio@gmail.com>
2018-11-27 09:24:59 -0500

    Removed api key

--- lib/tabs/second.dart
+++ lib/tabs/second.dart
@@ -17,7 +17,7 @@ class GetHttpDataState extends State<Second> {
 
     Object options = {
       // uncomment and add your API key
-      'key': "AIzaSyDYYUUSJGgWN_1BO6fK-_8AmRpTFJVYNwc",
+      // 'key': "INSERT YOUR KEY HERE",
       'part': "snippet",
       'maxResults': "25",
       'q': "unity3D VR",
